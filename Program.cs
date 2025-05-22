634eb9f0bc8f501ea84303cf8e86b68e97fa4e53
Larry Diego <larry_diego@hotmail.com>
2025-05-22 12:47:49 -0300

    feat: adicionado uma ideia para estrutura principal para chamada do menu

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Crud
     static void Main(string[] args)
     {
       System.Console.WriteLine("CRUD - STEAM GAMES KEYS by Larry Diego");
+      // Chamar Menu() ou criar o menu direto no Main()
     }
   }
 }
